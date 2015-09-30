85b08cf5c61dcb9e1637fc8c54f3d8b16e97acc0
Étienne WATTEBLED <etienne@wattebled.fr>
2015-09-30 06:42:16 +0200

    Update afficher.rb

--- samples/afficher.rb
+++ samples/afficher.rb
@@ -1,5 +1,9 @@
 i = 30
 puts "i=#{i}"
 puts i
-puts "La valeur est " + i
-puts "La valeur est " + i.to_s
+puts "La valeur de i est " + i
+puts "La valeur de i est " + i.to_s
+i = 10.5
+puts i
+puts "La valeur de i est" + i
+puts "La valeur de i est" + i.to_s
