852c90c20efff31c87b05fb3164d9b88e079b403
sahraoui62 <elouadi.slimane@yahoo.fr>
2015-10-02 11:47:23 +0200

    Ajout de test pour le modele

--- linky/app/models/link.rb
+++ linky/app/models/link.rb
@@ -1,6 +1,9 @@
 class Link < ActiveRecord::Base
   validates  :title, :url, :score, presence: true
-  validates :score, numericality: { greater_than_or_equal_to: 0 }
+  validates :score, numericality: { 
+  	greater_than_or_equal_to: 0,
+  	message: 'must be greater than 0'
+  }
   validates :title, uniqueness: true
   validates :url, allow_blank: false, format: {
     with: %r{\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z}i,
--- linky/test/models/link_test.rb
+++ linky/test/models/link_test.rb
@@ -9,4 +9,29 @@ class LinkTest < ActiveSupport::TestCase
     assert link.errors[:title].any?
     assert link.errors[:url].any?
   end
+
+  test "link validated" do
+	  link = Link.new(title: 'Link_title',url: 'http://www.google.fr')
+    assert link.valid?
+  end
+
+  test "score must be greater than 0" do
+    link = Link.new(title: 'Link_title',url: 'http://www.google.fr', score: -1)
+    assert link.invalid?
+    assert link.errors[:score].any?
+    puts link.errors[:score]
+  end
+
+  test "error with url format" do
+    link = Link.new(title: 'Link_title',url: 'google')
+    assert link.invalid?
+    assert link.errors[:url].any?
+    puts link.errors[:url]
+  end
+
+  test "check if score equals to 0 in initialisation" do
+    link = Link.new(title: 'Link_title',url: 'http://www.google.fr')
+    assert_equal( 0 , link[:score], 'the score equals to 0')
+  end  
+
 end
