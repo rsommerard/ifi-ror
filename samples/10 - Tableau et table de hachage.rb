8621824973b21a7bc9fcedcc8954c9cf26ba1478
Étienne WATTEBLED <etienne@wattebled.fr>
2015-10-03 16:56:53 +0200

    Create 10 - Tableau et table de hachage.rb

---
+++ samples/10 - Tableau et table de hachage.rb
@@ -0,0 +1,40 @@
+tab = ["Lucie","Léo","Juliette","Anne-Sophie","Antoine"]
+
+tab.each do |v|
+  puts v
+end
+
+s = "Amélie Julie Sophie Béatrice"
+tab = s.split(" ")
+
+tab[1]
+tab[2]
+
+tab.length
+
+tab = ["léa", "léo", "romain", "julie", "justine"]
+tab.each do |v|
+  v = v.capitalize
+  puts v
+end
+tab
+
+tab.length.times do |i|
+  tab[i] = tab[i].capitalize
+end
+tab
+
+tab = {
+  "prenom1" => "Léa",
+  "prenom2" => "Léo",
+  "prenom3" => "Antoine",
+  10 => "Julie",
+  20.3 => 6
+}
+
+tab["prenom1"]
+tab[10]
+tab[20.3]
+
+tab["prenom4"] = "Amélie"
+tab["prenom4"]
